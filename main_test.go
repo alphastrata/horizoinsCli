ea1cc2a411656d8e6ec02b70037627bd5d5197f0
jer <alphastrata@gmail.com>
2023-05-05 09:23:46 +1000

    fix: fix test

--- main_test.go
+++ main_test.go
@@ -2,31 +2,22 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-	"net/http/httptest"
 	"testing"
 )
 
 func TestHorizonsAPI_download(t *testing.T) {
-	// Create a test server that always returns the same response
-	expectedResponse := []byte("Test response")
-	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Write(expectedResponse)
-	}))
-	defer testServer.Close()
-
 	// Create a HorizonsAPI object with the test server's URL
 	api := &HorizonsAPI{
 		Format:     "text",
-		Command:    "'499'",
-		ObjData:    "'YES'",
-		MakeEphem:  "'YES'",
-		EphemType:  "'OBSERVER'",
-		Center:     "'500@399'",
-		StartTime:  "'2006-01-01'",
-		StopTime:   "'2006-01-20'",
-		StepSize:   "'1 d'",
-		Quantities: "'1,9,20,23,24,29'",
+		Command:    "499",
+		ObjData:    "YES",
+		MakeEphem:  "YES",
+		EphemType:  "OBSERVER",
+		Center:     "500@399",
+		StartTime:  "2006-01-01",
+		StopTime:   "2006-01-20",
+		StepSize:   "1%20d",
+		Quantities: "1,9,20",
 	}
 
 	// Make the API call and check that the response matches the expected response
@@ -34,5 +25,5 @@ func TestHorizonsAPI_download(t *testing.T) {
 	if err != nil {
 		t.Errorf("Error downloading data: %s", err)
 	}
-	fmt.Println(actualResponse)
+	fmt.Println(string(actualResponse))
 }
